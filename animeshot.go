dbee5c41b2377f721bc072b59949c1c75aa8bc91
A.I <merrick@luois.me>
2016-04-14 15:36:11 +0800

    add query via private message

--- animeshot.go
+++ animeshot.go
@@ -9,15 +9,25 @@ import (
 )
 
 func processUpdate(update telegram.Update) {
-	photos := search.SearchImageForKeyword(update.Inline_query.Query)
-
-	if len(photos) > 0 {
-		var images []telegram.InlineQueryResultPhoto
-		for _, photo := range photos {
-			result := telegram.InlineQueryResultPhoto{"photo", photo.Photo_url, photo.Photo_url, photo.Thumb_url, photo.Title, photo.Title, photo.Title}
-			images = append(images, result)
+	if len(update.Inline_query.Query) > 0 {
+		photos := search.SearchImageForKeyword(update.Inline_query.Query)
+		if len(photos) > 0 {
+			var images []telegram.InlineQueryResultPhoto
+			for _, photo := range photos {
+				result := telegram.InlineQueryResultPhoto{"photo", photo.Photo_url, photo.Photo_url, photo.Thumb_url, photo.Title, photo.Title, photo.Title}
+				images = append(images, result)
+			}
+			telegram.AnswerQuery(update.Inline_query.Id, images)
 		}
-		telegram.AnswerQuery(update.Inline_query.Id, images)
+	} else if len(update.Message.Text) > 0 {
+		photos := search.SearchImageForKeyword(update.Message.Text)
+		if len(photos) > 0 {
+			for _, photo := range photos {
+				telegram.SendMessage(update.Message.Chat.Id, photo.Title+" "+photo.Photo_url)
+			}
+		} else {
+			telegram.SendMessage(update.Message.Chat.Id, "No Search result > <")
+		}
 	}
 }
 
