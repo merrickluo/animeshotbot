a9e3977c2863b8805e1db9c265e321905f6b89f3
A.I <merrick@luois.me>
2017-02-07 22:54:55 +0800

    rewrite in clojure...

--- project.clj
+++ project.clj
@@ -3,7 +3,10 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.8.0"]]
+  :dependencies [[org.clojure/clojure "1.8.0"]
+                 [org.clojure/data.json "0.2.6"]
+                 [morse "0.2.3-SNAPSHOT"]]
+  :sub ["modules/morse"]
   :main ^:skip-aot animeshotbot.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})
--- src/animeshotbot/core.clj
+++ src/animeshotbot/core.clj
@@ -1,7 +1,38 @@
 (ns animeshotbot.core
   (:gen-class))
 
+(require '[morse.api :as api])
+(require '[morse.polling :as p])
+(require '[morse.handlers :refer :all])
+(require '[clojure.data.json :as json])
+(require '[clj-http.client :as client])
+
+(def token (System/getenv ))
+
+(defn search-shots
+  [shot-keyword]
+  (let [respBody ((client/get (str "https://as.bitinn.net/api/shots?q=" shot-keyword)
+                              {:insecure? true}) :body)]
+    (json/read-str respBody :key-fn keyword)))
+
+(defn build-inline-results
+  [shots]
+  (json/write-str (reduce conj [] (map (fn [shot] {:type "photo"
+                                                   :id (shot :sid)
+                                                   :photo_url (shot :image_large)
+                                                   :thumb_url (shot :image_thumbnail)
+                                                   :caption (shot :text)})
+                                       shots))))
+
+(defhandler bot-api
+  (message msg (let [shots (search-shots (:text msg))]
+                 (api/send-text token (get-in msg [:chat :id])
+                                (clojure.string/join "\n" (map :text shots)))))
+  (inline query (let [shots (search-shots (:query query))]
+                  (api/answer-inline token (:id query)
+                                     (build-inline-results shots)))))
+
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
-  (println "Hello, World!"))
+  (p/start token bot-api))
